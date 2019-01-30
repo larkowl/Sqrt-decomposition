27dc0536c4dbf75f7ae4e5bbfcc7f02bc07725c9
Эмилия Кибенко <emilyrakova@gmail.com>
2019-01-30 22:33:38 +0200

    Add files via upload

---
+++ console_input/cmake-build-debug/CMakeFiles/acocm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/acocm.dir/main.cpp.o"
+  "acocm.pdb"
+  "acocm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/acocm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
